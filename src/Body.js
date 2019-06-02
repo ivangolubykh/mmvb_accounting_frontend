9e52ac30b55d739f304cb6786966c60d9409d596
ivangolubykh <ivan9@allworld.xyz>
2019-06-02 05:49:22 +0300

    Dev [add] Add not authorizated page.

--- src/Body.js
+++ src/Body.js
@@ -1,17 +1,29 @@
 import React from 'react';
+
+import Alert from 'react-bootstrap/Alert';
+
 import BodyIssuers from './BodyIssuers';
 import BodyRegions from './BodyRegions';
 
 
 function Body( {mainParent} ) {
-  if (mainParent.state.currentPage === "#issuers") {
-    return <main className="container-fluid flex-grow-1"><BodyIssuers mainParent={mainParent} /></main>;
+  if (mainParent.state.isLogin === true) {
+    if (mainParent.state.currentPage === "#issuers") {
+      return <main className="container-fluid flex-grow-1"><BodyIssuers mainParent={mainParent} /></main>;
+    }
+    if (mainParent.state.currentPage === "#region") {
+      return <main className="container-fluid flex-grow-1"><BodyRegions mainParent={mainParent} /></main>;
+    }
   }
-  if (mainParent.state.currentPage === "#region") {
+
+  if ((mainParent.state.currentPage === "#home")  || (mainParent.state.currentPage === "")) {
     return <main className="container-fluid flex-grow-1"><BodyRegions mainParent={mainParent} /></main>;
   }
 
-  return <main className="container-fluid flex-grow-1"><BodyIssuers mainParent={mainParent} /></main>;
+  return (<main className="container-fluid flex-grow-1">
+      <Alert variant='danger'>Страница не найдена или не доступна. Возможно вы не авторизованы.</Alert>
+    </main>
+  );
 }
 
 
